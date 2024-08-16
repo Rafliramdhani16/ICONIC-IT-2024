a32ef984b3d45cf84b5be8e908971c7284068606
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-08-17 00:00:52 +0700

    fix: button

--- src/Components/Fragments/Card/CardModul.jsx
+++ src/Components/Fragments/Card/CardModul.jsx
@@ -2,18 +2,24 @@ import { useState } from "react";
 import { AiFillCaretRight, AiFillCaretLeft } from "react-icons/ai";
 import { BiArrowBack } from "react-icons/bi";
 import Sidebar from "../SideBar";
+import { useNavigate } from "react-router-dom";
 
 const CardModul = () => {
   const [open, setOpen] = useState(true);
   const toggleSidebar = () => {
     setOpen(!open);
   };
-
+  const navigate = useNavigate();
   return (
     <>
-      <nav className="flex items-center p-4 border-b border-neutral-300 shadow-sm rounded-b-3xl">
-        <BiArrowBack className="text-2xl text-neutral-800" />
-        <p className="text-xl font-semibold ml-2">Daftar Modul</p>
+      <nav className="flex items-center p-5 border-b border-neutral-300 shadow-sm rounded-b-3xl">
+        <button
+          className="flex items-center"
+          onClick={() => navigate("/detailMateri/:id")}
+        >
+          <BiArrowBack className="text-2xl text-neutral-800" />
+          <p className="text-xl font-semibold ml-2">Daftar Modul</p>
+        </button>
       </nav>
 
       <div className="flex flex-col lg:flex-row h-full">
